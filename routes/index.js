b6f4317b5eaae1f8e53a6a3b51c4c6e5ff3df690
ivan-rangel <ivan05rangel@gmail.com>
2015-06-27 15:15:19 -0500

    CRUD en Express

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+/*jslint node: true, indent: 2,nomen:true */
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*jslint unparam: true*/
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Primer pagina de express' });
+});
+/*jslint unparam: false*/
+
+module.exports = router;
